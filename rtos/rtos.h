f400edfc009cc824032509e377556142263898d8
Markus Mayer <development@widemeadows.de>
2013-05-27 23:51:57 +0200

    Eigenen Header für Thread-Funktionalität herausgezogen

--- rtos/rtos.h
+++ rtos/rtos.h
@@ -2,6 +2,7 @@
 #define RTOS_H
 
 #include "datatypes.h"
+#include "threads.h"
 
 /**
 * Initialisiert das Betriebssystem.
@@ -21,41 +22,6 @@ void initOS(void);
 * @see initOS()
 */
 void startOS(void);
-
-/**
-* Zeiger auf eine Threadfunktion.
-*/ 
-typedef void (thread_function_t)(void);
-
-/**
-* Thread-Nummer.
-*/
-typedef uint8_t threadno_t;
-
-/**
-* Definiert einen Fehlerfall bei der Threaderzeugung.
-*/
-#define THREAD_REGISTER_ERROR ((threadno_t)-1)
-
-/**
-* Beschreibt die Threadpriorität
-*/
-typedef enum {
-	PRIO_HIGHEST 					= 10,	 //< Höchste Priorität
-	PRIO_HIGH 						= 5,	 //< Hohe Priorität
-	PRIO_NORMAL  					= 0,	 //< Normale Priorität
-	PRIO_LOWEST 					= -10, //< Niedrigste Priorität
-	PRIO_RESERVED_IDLE 	  = -127 //< Niedrigste Priorität (reserviert für Idle-Thread)
-} thread_priority_t;
-
-/**
-* Registriert einen Thread.
-*
-* @param thread 	Funktionszeiger auf den Thread
-* @param priority Priorität des Threads
-* @returns 		  	Die Nummer des Threads oder THREAD_REGISTER_ERROR im Fehlerfall.
-*/
-threadno_t registerThread(thread_function_t* thread, thread_priority_t priority);
 
 #else /* RTOS_H */
 #error rtos.h mehrfach inkludiert
