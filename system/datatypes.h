b21bfea80d89138f88e84b884d721e473562eeb0
Markus Mayer <development@widemeadows.de>
2013-05-27 22:04:52 +0200

    Boolean-Datentyp hinzugefügt

--- system/datatypes.h
+++ system/datatypes.h
@@ -1,8 +1,13 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
-typedef unsigned char 		uint8_t;	// 8-bit  unsigned
-typedef unsigned short int 	uint16_t;	// 16-bit unsigned
+typedef unsigned char 		  uint8_t;		// 8-bit  unsigned
+typedef unsigned short int 	uint16_t;		// 16-bit unsigned
 typedef unsigned long int 	uint32_t;   // 32-bit unsigned
+
+typedef enum {
+	true = 1,
+	false = 0
+} bool;
 
 #endif /* DATATYPES_H */
