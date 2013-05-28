6ec14a130aa34ee3ee3e846ae702c7aa9d38a0cc
Markus Mayer <development@widemeadows.de>
2013-05-28 23:34:05 +0200

    Signed int

--- rtos/datatypes.h
+++ rtos/datatypes.h
@@ -1,6 +1,7 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
+typedef   signed char 		   int8_t;		// 8-bit    signed
 typedef unsigned char 		  uint8_t;		// 8-bit  unsigned
 typedef unsigned short int 	uint16_t;		// 16-bit unsigned
 typedef unsigned long int 	uint32_t;   // 32-bit unsigned
