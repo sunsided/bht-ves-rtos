d2b0dce2df6ed77bd41ad48ba30419090fb8eb46
Markus Mayer <development@widemeadows.de>
2013-05-28 23:22:07 +0200

    Schleife zum Umkopieren des Registerinhaltes

--- rtos_impl/rtos.c
+++ rtos_impl/rtos.c
@@ -62,7 +62,8 @@ void initOS(void)
 *****************************************************************************/
 timer0() interrupt 1 using 1						// Int Vector at 000BH, Reg Bank 1  
 {
-	static uint8_t idata * pi;				// Pointer in das interne RAM
+	static uint8_t regIdx;			// Register-Index in der Schleife
+	static uint8_t idata *pi;				// Pointer in das interne RAM
 	static uint8_t idata *pd = POSRB0;	// Pointer auf die Registerbank 0
 	static uint8_t CurrentThread = 0;	// Nr des laufenden Threads
 	static uint8_t NewThread = FIRST;	// Nr des naechsten Threads
@@ -72,7 +73,7 @@ timer0() interrupt 1 using 1						// Int Vector at 000BH, Reg Bank 1
 															// "if (NewThread == FIRST)").
 
 	reload_system_timer();
-	
+		
 	if (NrThreads > 0) {								// Sind Threads zu verwalten?
 
 		NewThread = (CurrentThread + 1)%NrThreads;	// Threadumschaltung
@@ -94,26 +95,20 @@ timer0() interrupt 1 using 1						// Int Vector at 000BH, Reg Bank 1
 				tcb[CurrentThread].sp  =  pi;			// Sichern des SP
 			}               
 
-			tcb[CurrentThread].reg[0]  = *(pd + 0);		// Retten von R0-R7 aus der
-			tcb[CurrentThread].reg[1]  = *(pd + 1);		// von allen Threads gemeinsam
-			tcb[CurrentThread].reg[2]  = *(pd + 2);		// genutzten Registerbank 0
-			tcb[CurrentThread].reg[3]  = *(pd + 3);    
-			tcb[CurrentThread].reg[4]  = *(pd + 4);    
-			tcb[CurrentThread].reg[5]  = *(pd + 5);    
-			tcb[CurrentThread].reg[6]  = *(pd + 6);
-			tcb[CurrentThread].reg[7]  = *(pd + 7);
+			// Retten von R0-R7 aus der von allen Threads gemeinsam genutzten Registerbank 0
+			for(regIdx=0; regIdx<REGISTER_COUNT; ++regIdx)
+			{
+				tcb[CurrentThread].reg[regIdx]  = *(pd + regIdx);
+			}
 			
 			SP = tcb[NewThread].sp;						// geretteten SP des Threads
 			pi = (unsigned char idata *)SP;			// in Pointer pi laden
 			
-			*(pd + 0) = tcb[NewThread].reg[0];			// Wiederherstellen von R0-R7
-			*(pd + 1) = tcb[NewThread].reg[1];			// in Registerbank 0
-			*(pd + 2) = tcb[NewThread].reg[2];
-			*(pd + 3) = tcb[NewThread].reg[3];
-			*(pd + 4) = tcb[NewThread].reg[4];
-			*(pd + 5) = tcb[NewThread].reg[5];
-			*(pd + 6) = tcb[NewThread].reg[6];
-			*(pd + 7) = tcb[NewThread].reg[7];
+			// Wiederherstellen von R0-R7 in Registerbank 0
+			for(regIdx=0; regIdx<REGISTER_COUNT; ++regIdx)
+			{
+				*(pd + regIdx) = tcb[NewThread].reg[regIdx];
+			}
 		
 			CurrentThread = NewThread;					// Ab jetzt ist der neue Thread
 		}                                         // der aktuelle!
