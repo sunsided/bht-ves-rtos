d2001ef0a0b1b8bae939276c3077f3b7207ce3bd
Markus Mayer <development@widemeadows.de>
2013-06-18 20:50:14 +0200

    Ringpuffer und Port-Toggling gemäß Aufgabe hinzugefügt

--- MT.c
+++ MT.c
@@ -25,7 +25,13 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <reg515c.h>
 #include "rtos/rtos.h"
+
+static bool zeitausgabe_aktiviert = false;
+
+#define BUFFER_SIZE 42U
+static volatile uint32_t shared_mem[BUFFER_SIZE];
 
 /**
 * Semaphor für den Systemzeit-Thread.
@@ -35,7 +41,8 @@ static semaphore_t timer_semaphore;
 /**
 * Test-Semaphore
 */
-static semaphore_t test_semaphore[2];
+static semaphore_t wert_da;
+static semaphore_t platz_da;
 
 // Alle Threads laufen in Registerbank 0
 void tick_system_time(void)
@@ -58,25 +65,49 @@ void print_system_time(void)
 	}
 }
 
-void thread2(void)
+void erzeuger(void)
 {
-	static int i = 0;
-	
+	static uint32_t wert = 0;
+	static uint8_t write_idx = 0;
+
 	while(1) {
-		i++;
-		os_sleep(0);
+		os_semaphore_wait(&platz_da);
+
+		shared_mem[write_idx] = wert++;
+		write_idx = (++write_idx)%BUFFER_SIZE;
+
+		if((wert%10)==0)
+		{
+			P5 |= 0x01; //Pin0 an
+			os_sleep(20);
+			P5 &= ~(0x01);
+		}
+
+		os_semaphore_post(&wert_da);
 	}
 }
 
-void thread3(void)
+void verbraucher(void)
 {
-	static int i = 0;
-	
+	static uint8_t read_idx = 0;
+	static uint32_t vergleichswert = 0;
+	static uint32_t wert;	
+
 	while(1) {
-		os_semaphore_wait(&test_semaphore);
-		
-		i++;
-		os_sleep(100);
+		os_semaphore_wait(&wert_da);
+
+		wert = shared_mem[read_idx];	
+		read_idx = (++read_idx)%BUFFER_SIZE;
+
+		 // werte müssten synchron sein
+		//assert(wert == vergleichswert);
+		if(wert != vergleichswert)
+		{
+			P5 |= 0x02;
+		}
+		vergleichswert++;
+
+		os_semaphore_post(&platz_da);
 	}
 }
 
@@ -92,21 +123,25 @@ void thread3(void)
 void register_threads() {
 	threadno_t thread;
 		
-	thread = os_register_thread(tick_system_time, PRIO_HIGHEST, "System Tick");
+	if (zeitausgabe_aktiviert) 
+	{
+		thread = os_register_thread(tick_system_time, PRIO_HIGHEST, "System Tick");
+		ASSERT_THREAD_REGISTERED(thread);
+		printf("tick_system_time registriert als ID %u.\r\n", (uint16_t)thread); // NOTE: Cast ist ein Fix für endianness
+		
+		thread = os_register_thread(print_system_time, PRIO_NORMAL, "Uhrzeit");
+		ASSERT_THREAD_REGISTERED(thread);
+		printf("print_system_time registriert als ID %u.\r\n", (uint16_t)thread);
+
+	}
+
+	thread = os_register_thread(erzeuger, PRIO_HIGH, "Erzeuger-Thread");
 	ASSERT_THREAD_REGISTERED(thread);
-	printf("tick_system_time registriert als ID %u.\r\n", (uint16_t)thread); // NOTE: Cast ist ein Fix für endianness
+	printf("erzeuger registriert als ID %u.\r\n", (uint16_t)thread);
 	
-	thread = os_register_thread(print_system_time, PRIO_HIGH, "Uhrzeit");
+	thread = os_register_thread(verbraucher, PRIO_NORMAL, "Verbraucher-Thread");
 	ASSERT_THREAD_REGISTERED(thread);
-	printf("print_system_time registriert als ID %u.\r\n", (uint16_t)thread);
-	
-	thread = os_register_thread(thread2, PRIO_LOW, "Dritter Thread");
-	ASSERT_THREAD_REGISTERED(thread);
-	printf("thread2 registriert als ID %u.\r\n", (uint16_t)thread);
-	
-	thread = os_register_thread(thread3, PRIO_NORMAL, "Vierter Thread");
-	ASSERT_THREAD_REGISTERED(thread);
-	printf("thread3 registriert als ID %u.\r\n", (uint16_t)thread);
+	printf("verbraucher registriert als ID %u.\r\n", (uint16_t)thread);
 }
 
 /**
@@ -121,21 +156,24 @@ void register_threads() {
 */
 void initialize_semaphores() {
 	sem_error_t result;
-		
+	
 	result = os_semaphore_init(&timer_semaphore, 0);
 	ASSERT_SEMAPHORE_INITIALIZED(result, timer_semaphore);
 	printf("Timer-Semaphor initialisiert als ID %u.\r\n", (uint16_t)timer_semaphore.semaphore_id);
 	
-	result = os_semaphore_init(&test_semaphore[0], 10);
-	ASSERT_SEMAPHORE_INITIALIZED(result, test_semaphore[0]);
-	printf("Test-Semaphor 1 initialisiert als ID %u.\r\n", (uint16_t)test_semaphore[0].semaphore_id);
+	result = os_semaphore_init(&wert_da, 00);
+	ASSERT_SEMAPHORE_INITIALIZED(result, wert_da);
+	printf("Semaphor \"wert_da\" initialisiert als ID %u.\r\n", (uint16_t)wert_da.semaphore_id);
 	
-	result = os_semaphore_init(&test_semaphore[1], 42);
-	ASSERT_SEMAPHORE_INITIALIZED(result, test_semaphore[1]);
-	printf("Test-Semaphor 2 initialisiert als ID %u.\r\n", (uint16_t)test_semaphore[1].semaphore_id);
+	result = os_semaphore_init(&platz_da, BUFFER_SIZE);
+	ASSERT_SEMAPHORE_INITIALIZED(result, platz_da);
+	printf("Semaphor \"platz_da\" initialisiert als ID %u.\r\n", (uint16_t)platz_da.semaphore_id);
 }
 
 void main(void) {
+
+	P5 = 0x0;
+
 	os_init();
 	register_threads();
 	initialize_semaphores();
